b36e662016ab8204d5ec4d2d63a8711e841c62ba
aikesha <37406006+aikesha@users.noreply.github.com>
2020-09-22 21:50:15 +0500

    Add files via upload

---
+++ test_move/src/test_joint5_square.cpp
@@ -0,0 +1,33 @@
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+#include <math.h>   //for sin()
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "joint5moving");
+    float t = 0.00;
+
+    ros::NodeHandle nh;
+
+    ros::Publisher pub2 = nh.advertise<std_msgs::Float64>("/robot/joint5_position_controller/command", 100);
+
+    ros::Rate loop_rate(10);
+    ros::Time startTime = ros::Time::now();
+
+    while (ros::ok()) {
+                  std_msgs::Float64 msg_to_send;
+                  t = ros::Time::now().toSec();
+                  if (t >= 15)
+                  {
+                      msg_to_send.data = 1;
+                  } else {
+                      msg_to_send.data = 0;
+                  }
+                   
+                  pub2.publish(msg_to_send);
+                  ROS_INFO("Moving joint 5 to position %f", msg_to_send.data);
+                  //i = i + 0.01;
+                  ros::spinOnce();
+                  loop_rate.sleep();
+    }
+   // return 0;
+}
